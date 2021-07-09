58463f9d7cc411ecda27cec8c32516b4b991cc70
vineeth123-png <vineethdvs123@gmail.com>
2021-07-09 11:34:18 +0530

    Added card component to show the drawn cards

--- src/Deck.js
+++ src/Deck.js
@@ -1,19 +1,53 @@
 import React, { Component } from "react";
+import Card from "./Card";
 import axios from "axios";
-const API_URL = "https://www.deckofcardsapi.com/api/deck/new/shuffle/";
+const API_BASE_URL = "https://www.deckofcardsapi.com/api/deck";
 class Deck extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			deck: null
+			deck: null,
+			drawn: []
 		};
+		this.getCard = this.getCard.bind(this);
 	}
 	async componentDidMount() {
-		let deck = await axios.get(API_URL);
+		let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`);
 		this.setState({ deck: deck.data });
 	}
+	async getCard() {
+		let { deck_id } = this.state.deck;
+		let cardUrl = `${API_BASE_URL}/${deck_id}/draw/`;
+		try {
+			let cardRes = await axios.get(cardUrl);
+			if (!cardRes.data.success) throw new Error("No cards remaining!");
+			let card = cardRes.data.cards[0];
+			console.log(card);
+			this.setState((st) => ({
+				drawn: [
+					...st.drawn,
+					{
+						id: card.code,
+						image: card.image,
+						name: `${card.suit} ${card.code}`
+					}
+				]
+			}));
+		} catch (err) {
+			alert(err);
+		}
+	}
 	render() {
-		return <h1>Card Dealer</h1>;
+		const cards = this.state.drawn.map((card) => (
+			<Card key={card.id} name={card.name} image={card.image} />
+		));
+		return (
+			<div>
+				<h1>Card Dealer</h1>
+				<button onClick={this.getCard}>Get a Card</button>
+				{cards}
+			</div>
+		);
 	}
 }
 
